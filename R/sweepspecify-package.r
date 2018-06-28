40a89b3f998a1d8eb03290e96a8907edbadf1f9e
Will Beasley <wibeasley@hotmail.com>
2018-06-28 15:14:48 -0500

    starting package skeleton
    ref #1

---
+++ R/sweepspecify-package.r
@@ -0,0 +1,36 @@
+#' sweepspecify
+#'
+#' @name sweepspecify
+#' @docType package
+#'
+#' @title Sweep & Specify Operations for the OUHSC CDW
+#'
+#' @description
+#'
+#' Thanks to Funders, ...
+#'
+#' @note
+#' The most recent development version is available through [GitHub](https://github.com/OuhscBbmc/sweepspecify) by
+#' running
+#' `devtools::install_github` `(repo = 'OuhscBbmc/sweepspecify')`
+#' (make sure [devtools](https://cran.r-project.org/package=devtools) is already installed).
+#' If you're having trouble with the package, please install the development version.  If this doesn't solve
+#' your problem, please create a [new issue](https://github.com/OuhscBbmc/sweepspecify/issues), or email Will.
+#'
+#' @author
+#'   [William Howard Beasley](http://scholar.google.com/citations?user=ffsJTC0AAAAJ),
+#'   University of Oklahoma Health Sciences Center, College of Medicine,
+#'   Dept of Pediatrics, [BBMC](http://www.ouhsc.edu/bbmc/#team).
+#'
+#' Maintainer: Will Beasley <wibeasley@@hotmail.com>
+#'
+#' @examples
+#' \dontrun{
+#' # Install/update sweepspecify with the release version from CRAN.
+#' install.packages('sweepspecify') #But it's not on CRAN yet.
+#'
+#' # Install/update sweepspecify with the development version from GitHub
+#' #install.packages('devtools') #Uncomment if `devtools` isn't installed already.
+#' devtools::install_github('OuhscBbmc/sweepspecify')
+#' }
+NULL
