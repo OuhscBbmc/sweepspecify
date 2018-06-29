22a599d69a3d389428ca278c5e864f0027517470
Will Beasley <wibeasley@hotmail.com>
2018-06-28 21:59:01 -0500

    remove reference to OuhscMunge package
    (ie, the package I copied this dummy function from).
    
    ref #1

--- tests/testthat/test-trim.R
+++ tests/testthat/test-trim.R
@@ -11,8 +11,8 @@ test_that("trim_numeric -good", {
   expected_1  <- c(NA, NA, NA, NA, NA, -.15, -0.1, 0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, NA, NA, NA)
   expected_2  <- c(NA, 99, 99, 99, 99, -.15, -0.1, 0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 99, 99, 99)
 
-  observed_1  <- OuhscMunge::trim_numeric(x, bounds)
-  observed_2  <- OuhscMunge::trim_numeric(x, bounds, replacement)
+  observed_1  <- trim_numeric(x, bounds)
+  observed_2  <- trim_numeric(x, bounds, replacement)
 
   expect_equal(observed_1, expected_1)
   expect_equal(observed_2, expected_2)
@@ -24,8 +24,8 @@ test_that("trim_numeric -good", {
 test_that("trim_numeric -degenerate", {
   bounds      <- c( -.15, .8)
 
-  expect_equal(OuhscMunge::trim_numeric(NA_real_, bounds), NA_real_)
-  expect_equal(OuhscMunge::trim_numeric(numeric(0), bounds), numeric(0))
+  expect_equal(trim_numeric(NA_real_, bounds), NA_real_)
+  expect_equal(trim_numeric(numeric(0), bounds), numeric(0))
 })
 
 test_that("trim_numeric -bad values", {
@@ -34,16 +34,16 @@ test_that("trim_numeric -bad values", {
   replacement <- 99
 
   expect_error(
-    OuhscMunge::trim_numeric(x, NA_real_),
+    trim_numeric(x, NA_real_),
     "Assertion on 'bounds' failed: Must have length >= 2, but has length 1."
   )
   expect_error(
-    OuhscMunge::trim_numeric(x, c(6, 2)),
+    trim_numeric(x, c(6, 2)),
     "The lower element of `bounds` must be equal or less than the upper element of `bounds`."
   )
 
   expect_error(
-    OuhscMunge::trim_numeric(x, c(1, 2, 3)),
+    trim_numeric(x, c(1, 2, 3)),
     "Assertion on 'bounds' failed: Must have length <= 2, but has length 3."
   )
 })
